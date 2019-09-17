79c2f3115d36a134ebfb56c1be8d3e8c40077fbb
zhb <1311470691@qq.com>
2019-09-17 18:51:51 +0800

    a

--- app/Http/Controllers/GoodsController.php
+++ app/Http/Controllers/GoodsController.php
@@ -10,7 +10,12 @@ class GoodsController extends Controller
     public function getGoodsInfo(){
         $goodsModel = new Goods();
         $page_size = 10;
+        $page = $_POST['page']??1;
         $goodsInfo = $goodsModel->paginate($page_size);
-        dd($goodsInfo);
+        if($goodsInfo){
+            return ['status'=>200,'msg'=>'ok','data'=>$goodsInfo];
+        }else{
+            return ['status'=>200,'msg'=>'ok','data'=>[]];
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,4 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/getGoodsInfo','GoodsController@getGoodsInfo');
+Route::post('/getGoodsInfo','GoodsController@getGoodsInfo');
