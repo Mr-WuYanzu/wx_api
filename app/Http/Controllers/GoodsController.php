431020edfe0915e11cd1b3f0c3859f5516fe5973
zhb <1311470691@qq.com>
2019-09-17 19:05:37 +0800

    a

--- app/Http/Controllers/GoodsController.php
+++ app/Http/Controllers/GoodsController.php
@@ -12,10 +12,11 @@ class GoodsController extends Controller
         $page_size = 10;
         $page = $_POST['page']??1;
         $goodsInfo = $goodsModel->paginate($page_size);
+        $lastPage = $goodsInfo->lastPage();
         if($goodsInfo){
-            return ['status'=>200,'msg'=>'ok','data'=>$goodsInfo];
+            return ['status'=>200,'msg'=>'ok','data'=>$goodsInfo,'lastPage'=>$lastPage];
         }else{
-            return ['status'=>200,'msg'=>'ok','data'=>[]];
+            return ['status'=>200,'msg'=>'ok','data'=>[],'lastPage'=>$page-1];
         }
     }
 }
